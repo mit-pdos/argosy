33e5f03e13dd1b944e9432d508ff18a57bc8d285
Tej Chajed <tchajed@mit.edu>
2019-02-19 10:28:59 -0500

    Add [Print Assumptions] to ComposedRefinement.v

--- src/Examples/Logging/ComposedRefinement.v
+++ src/Examples/Logging/ComposedRefinement.v
@@ -11,6 +11,7 @@ Module LoggingTwoDiskRefinement.
   Definition rf : LayerRefinement TwoDisk.TDLayer TxnD.l :=
     refinement_transitive ReplicatedDisk.Refinement_TD_OD LoggingRefinement.rf.
   Check rf.(compile_exec_seq_ok).
+  Print Assumptions rf.
   Definition compile := rf.(compile).
   Definition init := rf.(init).
   Definition recover := rf.(recover).

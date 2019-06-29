92f692603666a85565a029cb8364e0853843697a
Tej Chajed <tchajed@mit.edu>
2019-06-29 12:23:35 -0700

    Auto-format Haskell with brittany

--- logging-client/src/Replication/TwoDiskEnvironment.hs
+++ logging-client/src/Replication/TwoDiskEnvironment.hs
@@ -1,6 +1,5 @@
 module Replication.TwoDiskEnvironment
-  (
-    Proc
+  ( Proc
   , Env
   , disk0
   , disk1
@@ -8,14 +7,19 @@ module Replication.TwoDiskEnvironment
   , runTD
   , (>>=)
   , return
-  ) where
+  )
+where
 
-import Control.Concurrent.MVar (MVar, newEmptyMVar)
-import Control.Monad.Reader (ReaderT, runReaderT)
-import System.Directory (doesFileExist)
-import System.IO.Error
-import System.Posix.IO
-import System.Posix.Types
+import           Control.Concurrent.MVar        ( MVar
+                                                , newEmptyMVar
+                                                )
+import           Control.Monad.Reader           ( ReaderT
+                                                , runReaderT
+                                                )
+import           System.Directory               ( doesFileExist )
+import           System.IO.Error
+import           System.Posix.IO
+import           System.Posix.Types
 
 type CachedHandle = (FilePath, Maybe Fd)
 
@@ -26,12 +30,10 @@ data Env =
 type Proc = ReaderT Env IO
 
 getFd :: CachedHandle -> IO (Maybe Fd)
-getFd (_, Nothing) = return Nothing
+getFd (_ , Nothing) = return Nothing
 getFd (fn, Just fd) = do
   exists <- doesFileExist fn
-  if exists
-    then return (Just fd)
-    else return Nothing
+  if exists then return (Just fd) else return Nothing
 
 disk0 :: Env -> IO (Maybe Fd)
 disk0 = getFd . disk0Handle
@@ -40,17 +42,15 @@ disk1 :: Env -> IO (Maybe Fd)
 disk1 = getFd . disk1Handle
 
 newEnv :: FilePath -> FilePath -> IO Env
-newEnv fn0 fn1 = pure Env
-  <*> openFile fn0
-  <*> openFile fn1
-  where openFile :: FilePath -> IO CachedHandle
-        openFile path =
-          catchIOError (do
-              fd <- openFd path ReadWrite Nothing defaultFileFlags
-              return (path, Just fd))
-          (\e -> if isDoesNotExistError e
-                 then return (path, Nothing)
-                 else ioError e)
+newEnv fn0 fn1 = pure Env <*> openFile fn0 <*> openFile fn1
+ where
+  openFile :: FilePath -> IO CachedHandle
+  openFile path = catchIOError
+    (do
+      fd <- openFd path ReadWrite Nothing defaultFileFlags
+      return (path, Just fd)
+    )
+    (\e -> if isDoesNotExistError e then return (path, Nothing) else ioError e)
 
 
 runTD :: Env -> Proc a -> IO a
--- logging-client/src/Transactions/LogEncoding.hs
+++ logging-client/src/Transactions/LogEncoding.hs
@@ -3,52 +3,58 @@ module Transactions.LogEncoding
   , descriptorFmt
   , encodeNum
   , decodeNum
-  ) where
+  )
+where
 
-import qualified Data.ByteString.Lazy as BSL
-import qualified Data.ByteString as BS
-import Utils.Conversion
-import Data.Binary.Put (runPut, putWord32le)
-import Data.Binary.Get (runGet, getWord32le)
-import Data.Binary
-import Control.Monad (replicateM)
-import Disk (Coq_block)
+import qualified Data.ByteString.Lazy          as BSL
+import qualified Data.ByteString               as BS
+import           Utils.Conversion
+import           Data.Binary.Put                ( runPut
+                                                , putWord32le
+                                                )
+import           Data.Binary.Get                ( runGet
+                                                , getWord32le
+                                                )
+import           Data.Binary
+import           Control.Monad                  ( replicateM )
+import           Disk                           ( Coq_block )
 
-import LogEncoding
+import           LogEncoding
 
 -- output must be exactly blocksize bytes
 toBlock :: Put -> Coq_block
-toBlock b = let bs = BSL.toStrict (runPut b)
-                padding = BS.replicate (blocksize - BS.length bs) 0 in
-                if BS.length bs > blocksize
-                  then error "string is already too long"
-                  else BS.append bs padding
+toBlock b =
+  let bs      = BSL.toStrict (runPut b)
+      padding = BS.replicate (blocksize - BS.length bs) 0
+  in  if BS.length bs > blocksize
+        then error "string is already too long"
+        else BS.append bs padding
 
 encodeLog :: LogHdr -> Coq_block
 encodeLog (Build_LogHdr b n) =
-  let binary = putWord8 (if b then 1 else 0) >> putWord32le (fromIntegral n) in
-      toBlock binary
+  let binary = putWord8 (if b then 1 else 0) >> putWord32le (fromIntegral n)
+  in  toBlock binary
 
 decodeLog :: Coq_block -> LogHdr
 decodeLog bs =
   let fmt = do
         b <- getWord8
         n <- getWord32le
-        return $ Build_LogHdr (b == 1) (fromIntegral n) in
-    runGet fmt (BSL.fromStrict bs)
+        return $ Build_LogHdr (b == 1) (fromIntegral n)
+  in  runGet fmt (BSL.fromStrict bs)
 
 logHdrFmt :: Coq_block_encoder LogHdr
 logHdrFmt = Build_block_encoder encodeLog decodeLog
 
 encodeDesc :: Descriptor -> Coq_block
 encodeDesc ds =
-  let binary = mapM_ (putWord32le . fromIntegral) ds in
-    toBlock binary
+  let binary = mapM_ (putWord32le . fromIntegral) ds in toBlock binary
 
 decodeDesc :: Coq_block -> Descriptor
 decodeDesc bs =
-  let fmt = replicateM (fromIntegral coq_LOG_LENGTH) (fromIntegral <$> getWord32le) in
-    runGet fmt (BSL.fromStrict bs)
+  let fmt = replicateM (fromIntegral coq_LOG_LENGTH)
+                       (fromIntegral <$> getWord32le)
+  in  runGet fmt (BSL.fromStrict bs)
 
 descriptorFmt :: Coq_block_encoder Descriptor
 descriptorFmt = Build_block_encoder encodeDesc decodeDesc
@@ -58,4 +64,4 @@ encodeNum :: Int -> Coq_block
 encodeNum = toBlock . putWord32le . fromIntegral
 
 decodeNum :: Coq_block -> Int
-decodeNum  = runGet (fromIntegral <$> getWord32le) . BSL.fromStrict
+decodeNum = runGet (fromIntegral <$> getWord32le) . BSL.fromStrict
